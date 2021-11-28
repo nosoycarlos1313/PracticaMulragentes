dc998961dd9981d4a6321caa18cc3f90deaa5d3b
rodrigoec11 <90631698+rodrigoec11@users.noreply.github.com>
2021-11-28 13:29:42 +0100

    Update Comprador.js

--- Comprador.js
+++ Comprador.js
@@ -1,22 +1,39 @@
-class Comprador {
 
-    //Constructor en el que me he basado
-    constructor(ip, puerto, ipMonitor, puertoMonitor, log) {
+class Comprador {
+  constructor(ip, puerto, ipMonitor, puertoMonitor, log) {
+    
+		//necesarios para las comunicaciones
+    
 		this.ip = ip;
 		this.puerto = puerto;
 		this.ipMonitor = ipMonitor;
 		this.puertoMonitor = puertoMonitor;
 		this.id = 0;
+     //
 		this.listaCompra = [];
 		this.listaTiendas = [];
-		this.tiempoConsumido = 0;
-		this.log = log;
 
-		this.GestorMensajes = new MessageManager(ipMonitor, puertoMonitor, ip, log);
+		this.GestorMensajes = new MessageManager(ipMonitor, puertoMonitor, ip);//, log);
+  }
+
+    async senalaEntrada(tiendaActual) {
+        //para generar el XML del mensaje InfoMensaje
+		var infoM = {
+			tipo_mensaje: 'entrada_tienda',
+			tipo_receptor: 'tienda',
+			id_receptor: this.listaTiendas[tiendaActual].id_tienda,
+			ip_receptor: this.listaTiendas[tiendaActual].ip_tienda,
+			puerto_receptor: this.listaTiendas[tiendaActual].puerto_tienda,
+			productos: this.listaCompra,
+			tiendas: this.listaTiendas
+		}
+        //esperas la respuesta de la tienda
+		var respuesta = await this.GestorMensajes.enviarXML(infoM);
+		return respuesta;
 	}
 
-    //Método para ver si quedan productos por comprar
-    productsLeft(){
+   //Método para ver si quedan productos por comprar
+   productsLeft(){
 
         if (this.listaCompra.length == 0){
             return false;
@@ -30,6 +47,20 @@ class Comprador {
             
         return false;
     }
+
+    reduceProductsQuantity(id, quantity) {
+        for (var i = 0; i < this.listaCompra.length; i++) {
+          //Si encuentra el producto con la misma id en la lista de la compra del cliente le quita la cantidad especifica
+          if (this.listaCompra[i].id_producto == id && parseFloat(this.listaCompra[i].cantidad) > 0) {
+            if (parseFloat(this.listaCompra[i].cantidad) < parseFloat(quantity)){
+              this.listaCompra[i].cantidad = "0";
+            }else{
+              this.listaCompra[i].cantidad = this.listaCompra[i].cantidad - quantity;
+            }
+            break;
+          }
+        }
+      }
     
     //Método para añadir al Log los eventos que van ocurriendo
     addToLog(string) {
@@ -138,6 +169,86 @@ class Comprador {
         return respuesta;
     }
     
+     
+    //Agrega de la lista de tiendas pasada, las tiendas que no conozca el cliente
+	agregarTiendas(tiendas) {
+		// Suponemos que la lista tiendas que nos pasan no tiene duplicados de tiendas
+		var tiendaActual = {};
+
+		//Tiendas por añadir
+		var tiendasNuevas = [];
+
+		//Saca las tiendas
+		for (var i = 0; i < tiendas.length; i++) {
+			tiendaActual = tiendas[i];
+
+			var isNueva = true;
+			//Comprueba si la tienda está en las listas conocidas
+			for (var j = 0; j < this.listaTiendas.length; j++) {
+
+				if (tiendaActual.id_tienda == this.listaTiendas[j].id_tienda) {
+					isNueva = false;
+					break;
+				}
+			}
+
+			//Si la tienda es nueva, se introduce a la lista de tienda y a la lista de tiendas nuevas
+			if (isNueva) {
+				this.listaTiendas.push(tiendaActual);
+				tiendasNuevas.push(tiendaActual);
+			}
+		}
+
+		//Mensaje de log correspondiente
+		// console.log(tiendasNuevas)
+		return this.listaTiendas
+	}
+
+
+    //Pide productos y tiendas
+    async pideIni() {
+		var mensaje = {
+			tipo_emisor:'comprador',
+			id_emisor: this.id,
+			ip_emisor: this.ip,
+			puerto_emisor: this.puerto,
+			tipo_receptor: 'monitor',
+			ip_monitor: this.ipMonitor,
+			puerto_monitor: this.puertoMonitor,//30
+			protocolo:'inicio',
+			tipo: 'MEI',
+		}
+
+		var respuesta = await this.GestorMensajes.enviarXML(mensaje);
+
+
+        if (respuesta != -1) {
+			this.listaCompra = respuesta['body']['lista_productos'];
+			this.listaTiendas = respuesta['body']['lista_tiendas'];
+			this.id = respuesta['head']['id_receptor'];
+			this.tiempoConsumido = this.tiempoConsumido + respuesta['head']['time_sent']
+        }
+
+		return respuesta;
+	}
+
+
+    async salidaTienda(tiendaActual) {
+		var infoM = {
+			tipo_mensaje: 'salida_tienda',
+            tipo_emisor:'comprador',
+			id_emisor: this.id,
+			ip_emisor: this.ip,
+			puerto_emisor: this.puerto,
+			tipo_receptor: 'tienda',
+			id_receptor: this.listaTiendas[tiendaActual].id_tienda,
+			ip_receptor: this.listaTiendas[tiendaActual].ip_tienda,
+			puerto_receptor: this.listaTiendas[tiendaActual].puerto_tienda,
+		}
+		var respuesta = await this.GestorMensajes.enviarXML(infoM)
+		return respuesta;
+	}  
+  
     //Método que controla la funcionalidad de los compradores
     async run() {
 
@@ -275,5 +386,6 @@ class Comprador {
     
         //Desconectamos al cliente
         this.finalizarCliente();
-    }
-}
+    } 
+       
+     }
